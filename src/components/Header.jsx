b66310d910560946dd42deda935b9e5cd7f43939
EDZA09 <eduarstiven2001@gmail.com>
2024-01-19 11:38:48 -0500

    Feat(Header): Desestructuro la prop setIsValidPresupuesto y la otorgo a NuevoPresupuesto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,18 @@
 import { NuevoPresupuesto } from "./NuevoPresupuesto";
 
-export const Header = ({ presupuesto, setPresupuesto }) => {
+export const Header = ({
+  presupuesto,
+  setPresupuesto,
+  isValidPresupuesto,
+  setIsValidPresupuesto,
+}) => {
   return (
     <header>
       <h1>Planificador de Gastos</h1>
       <NuevoPresupuesto
         presupuesto={presupuesto}
         setPresupuesto={setPresupuesto}
+        setIsValidPresupuesto={setIsValidPresupuesto}
       />
     </header>
   );
