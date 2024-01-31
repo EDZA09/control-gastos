4544ca9682efc27fed87a9b4cc1a5081b64f3429
EDZA09 <eduarstiven2001@gmail.com>
2024-01-31 11:17:10 -0500

    Feat(Modal): Importo un icono y lo inserto en la etiqueta img

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import CerrarBtn from "../assets/img/cerrar.svg";
 
 export const Modal = () => {
   return (
     <div className="modal">
       <p>Desde Modal</p>
+      <div className="cerrar-modal">
+        <img src={CerrarBtn} alt="cerrar modal" onClick={ocultarModal} />
+      </div>
     </div>
   );
 };
