5689299194079337dbfac8833d94807950b9f08c
EDZA09 <eduarstiven2001@gmail.com>
2024-02-09 12:00:40 -0500

    Feat(Modal): Añado los campos nombre del gasto, cantidad y categoria en el formulario

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -17,6 +17,40 @@ export const Modal = ({ setModal, animarModal, setAnimarModal }) => {
 
       <form className={`formulario ${animarModal ? "animar" : "cerrar"}`}>
         <legend>Nuevo Gasto</legend>
+        <div className="campo">
+          <label htmlFor="nombre">Nombre Gasto</label>
+
+          <input
+            type="text"
+            id="nombre"
+            placeholder="Añade el Nombre del Gasto"
+          />
+        </div>
+
+        <div className="campo">
+          <label htmlFor="cantidad">Cantidad</label>
+
+          <input
+            type="number"
+            id="cantidad"
+            placeholder="Añade la Cantidad del Gasto: ej. 300"
+          />
+        </div>
+
+        <div className="campo">
+          <label htmlFor="categoria">Cantidad</label>
+
+          <select id="categoria">
+            <option value="">-- Seleccione --</option>
+            <option value="ahorro">Ahorro</option>
+            <option value="comida">Comida</option>
+            <option value="casa">Casa</option>
+            <option value="gastos">Gastos Varios</option>
+            <option value="ocio">Ocio</option>
+            <option value="salud">Salud</option>
+            <option value="suscripciones">Suscripciones</option>
+          </select>
+        </div>
       </form>
     </div>
   );
