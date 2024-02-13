0a7a877f41dadb8ff64e1e36ca0c5b622aa620c3
EDZA09 <eduarstiven2001@gmail.com>
2024-02-13 13:24:09 -0500

    Feat(ListadoGastos): Importo y utilizo el componente Gasto

--- src/components/ListadoGastos.jsx
+++ src/components/ListadoGastos.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import Gasto from "./Gasto";
 
 const ListadoGastos = ({ gastos }) => {
   return (
     <div className="listado-gastos contenedor">
       <h2>{gastos.length ? "Gastos" : "No Hay Gastos aún"}</h2>
+      {gastos.map((gasto) => {
+        <Gasto />;
+      })}
     </div>
   );
 };
