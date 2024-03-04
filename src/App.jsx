386def45e46d23bba198834eafa3ea66a3d03df1
EDZA09 <eduarstiven2001@gmail.com>
2024-03-03 23:09:24 -0500

    Feat(App): Cambio la validación para fijar la ventana modal

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,7 @@ function App() {
   };
 
   return (
-    <div className={modal && "fijar"}>
+    <div className={modal ? "fijar" : ""}>
       <Header
         gastos={gastos}
         presupuesto={presupuesto}
