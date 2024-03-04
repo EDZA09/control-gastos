62205ce0467056bf5d00c4cccabcd2cdf6cf07c4
EDZA09 <eduarstiven2001@gmail.com>
2024-03-03 23:03:05 -0500

    Feat(ControlPresupuesto): Otorgo la propiedad gastos a Header, luego a ControlPresupuesto

--- src/App.jsx
+++ src/App.jsx
@@ -39,6 +39,7 @@ function App() {
   return (
     <div className={modal && "fijar"}>
       <Header
+        gastos={gastos}
         presupuesto={presupuesto}
         setPresupuesto={setPresupuesto}
         isValidPresupuesto={isValidPresupuesto}
