34a8a5ea93a60cc62d7c7b24c748be2a699bd122
EDZA09 <eduarstiven2001@gmail.com>
2024-03-05 13:39:03 -0500

    Feat(components/Gasto): Utilizo los componentes SwipeableList y SwipeableListItem de react-swipeable-list

--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -29,20 +29,24 @@ const diccionarioIconos = {
 const Gasto = ({ gasto }) => {
   const { categoria, nombre, cantidad, id, fecha } = gasto;
   return (
-    <div className="gasto sombra">
-      <div className="contenido-gasto">
-        <img src={diccionarioIconos[categoria]} alt="IconoGasto" />
-        <div className="descripcion-gasto">
-          <p className="categoria">{categoria}</p>
-          <p className="nombre-gasto">{nombre}</p>
-          <p className="fecha-gasto">
-            Agregado el: {""}
-            <span>{formatearFecha(fecha)}</span>
-          </p>
+    <SwipeableList>
+      <SwipeableListItem>
+        <div className="gasto sombra">
+          <div className="contenido-gasto">
+            <img src={diccionarioIconos[categoria]} alt="IconoGasto" />
+            <div className="descripcion-gasto">
+              <p className="categoria">{categoria}</p>
+              <p className="nombre-gasto">{nombre}</p>
+              <p className="fecha-gasto">
+                Agregado el: {""}
+                <span>{formatearFecha(fecha)}</span>
+              </p>
+            </div>
+          </div>
+          <p className="cantidad-gasto">${cantidad}</p>
         </div>
-      </div>
-      <p className="cantidad-gasto">${cantidad}</p>
-    </div>
+      </SwipeableListItem>
+    </SwipeableList>
   );
 };
 
